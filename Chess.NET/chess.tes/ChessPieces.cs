e80ed112e31d5b81a241b191ffd59338687b9433
kordiP <nasikordov@gmail.com>
2023-01-13 09:03:39 +0200

    Made a ChessPiece class, that controls the board and made thing more readable. In addition, there was a move tracker added.

---
+++ Chess.NET/chess.tes/ChessPieces.cs
@@ -0,0 +1,96 @@
+﻿using System.Text;
+
+namespace chess.tes
+{
+    internal class ChessPieces
+    {
+        public enum ChessPiece
+        {
+            None, Rook, Knig, Bish, Quen, King, Pawn
+        };
+        public ChessPiece[,] board;
+        public List<string> moves;
+        public ChessPieces()
+        {
+            this.board = new ChessPiece[8, 8];
+            moves = new List<string>();
+        }
+        public void GenerateChessBoard() // works :>
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                board[j, 1] = ChessPiece.Pawn;
+            }
+            board[0, 0] = ChessPiece.Rook; board[1, 0] = ChessPiece.Knig; board[2, 0] = ChessPiece.Bish;
+
+            board[3, 0] = ChessPiece.Quen; board[4, 0] = ChessPiece.King;
+      
+            board[5, 0] = ChessPiece.Bish; board[6, 0] = ChessPiece.Knig; board[7, 0] = ChessPiece.Rook;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 2; j < 6; j++)
+                {
+                    board[i, j] = ChessPiece.None;
+                }
+            }
+            board[0, 7] = ChessPiece.Rook; board[1, 7] = ChessPiece.Knig; board[2, 7] = ChessPiece.Bish;
+
+            board[3, 7] = ChessPiece.Quen; board[4, 7] = ChessPiece.King;
+
+            board[5, 7] = ChessPiece.Bish; board[6, 7] = ChessPiece.Knig; board[7, 7] = ChessPiece.Rook;
+            for (int j = 0; j < 8; j++)
+            {
+                board[j, 6] = ChessPiece.Pawn;
+            }
+        }
+        public string PrintChessBoard() // works :>
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 7; i >= 0; i--)
+            {
+                stringBuilder.AppendLine("\n+------+------+------+------+------+------+------+------+");
+                for (int j = 0; j < 8; j++)
+                {
+                    stringBuilder.Append("| " + board[j, i] + " ");
+                }
+                stringBuilder.Append("| " + (i + 1));
+            }
+            stringBuilder.AppendLine("\n+------+------+------+------+------+------+------+------+\n  a      b      c      d      e      f      g      h");
+            return stringBuilder.ToString();        
+        }
+        public bool IsLegalMove(int currX, int currY, int nextX, int nextY)
+        {
+            ChessPiece piece = board[currX, currY];
+            switch (piece)
+            {
+                case ChessPiece.None:
+                    return false;
+                case ChessPiece.Rook: // only in the same row OR the same column as the current position
+                    break;
+                case ChessPiece.Knig: // L position moves - hardest prolly
+                    break;
+                case ChessPiece.Bish: // diagonals
+                    break;
+                case ChessPiece.Quen: // diags + rows/columns
+                    break;
+                case ChessPiece.King: // 1 position in each dirrection
+                    break;
+                case ChessPiece.Pawn: // 1 postion on column, 2 is possible if at start
+                    break;
+                default:
+                    return true;
+            }
+            return true;
+        }
+        public void MovePiece(int currX, int currY, int nextX, int nextY)
+        {
+            ChessPiece piece = board[currX, currY];
+            if (IsLegalMove(currX, currY, nextX, nextY))
+            {
+                board[nextX, nextY] = piece;
+                board[currX, currY] = ChessPiece.None;
+                moves.Add($"{moves.Count + 1}.{(char)(currX + 97)}{currY + 1} -> {(char)(nextX + 97)}{nextY + 1}\t");
+            }
+        }
+    }
+}
--- Chess.NET/chess.tes/Engine.cs
+++ Chess.NET/chess.tes/Engine.cs
@@ -4,109 +4,39 @@ namespace chess.tes
 {
     internal class Engine
     {
-        public static void Run()
+        public static void Run() // type END for halt
         {
-            ChessPieces[,] board = new ChessPieces[8, 8];
-            GenerateChessBoard(board);
-            PrintChessBoard(board);
-            Console.WriteLine("Which piece do you want to move? /type a1, b1, d3 etc/ :");
-            string pieceCurrPos = Console.ReadLine();
-            int pieceCurrPosX = char.Parse(pieceCurrPos.Substring(0,1)) - 97;
-            int pieceCurrPosY = int.Parse(pieceCurrPos.Substring(1,1)) - 1;
-            Console.WriteLine(pieceCurrPosX + "-" + pieceCurrPosY);
-            ChessPieces selectedPiece = FindPiece(board, pieceCurrPosX, pieceCurrPosY);
-            Console.WriteLine("Where do you want it to move? /type a1, b1, d3 etc/ :");
-            string pieceNextPos = Console.ReadLine();
-            int pieceNextPosX = char.Parse(pieceNextPos.Substring(0, 1)) - 97; // -97 because character 'a' sits at position 97
-            int pieceNextPosY = int.Parse(pieceNextPos.Substring(1, 1)) - 1; // -1 because a1 = 0-0
-            MovePiece(board, selectedPiece, pieceNextPosX, pieceNextPosY);
-            PrintChessBoard(board);
-        }
-        enum ChessPieces
-        {
-            None, Rook, Knig, Bish, Quen, King, Pawn
-        };
-        static void PrintChessBoard(ChessPieces[,] chessBoard)
-        {
-            for (int i = 7; i >= 0; i--)
+            const string endCom = "END";
+            ChessPieces chess = new ChessPieces();
+            chess.GenerateChessBoard();
+            Console.WriteLine("Press enter to begin the game.");
+            string input = Console.ReadLine();
+            while (input != endCom)
             {
-                Console.WriteLine("\n+------+------+------+------+------+------+------+------+");
-                for (int j = 0; j < 8; j++)
-                {
-                    Console.Write("| " + chessBoard[j, i] + " ");
-                }
-                Console.Write("| " + (i + 1));
+                Console.WriteLine(chess.PrintChessBoard());
+                Console.WriteLine("Move piece at pos: ");
+                input = Console.ReadLine();
+                int currX = char.Parse(input.Substring(0, 1)) - 97;
+                int currY = int.Parse(input.Substring(1, 1)) - 1;
+                Console.WriteLine("Move piece to pos: ");
+                input = Console.ReadLine();
+                int nextX = char.Parse(input.Substring(0, 1)) - 97;
+                int nextY = int.Parse(input.Substring(1, 1)) - 1;
+                chess.MovePiece(currX, currY, nextX, nextY);
+                Console.Clear();
+                Console.WriteLine(chess.PrintChessBoard());
+                Console.WriteLine("Press enter for next move or type /END/ to close.");
+                input = Console.ReadLine();
             }
-            Console.WriteLine("\n+------+------+------+------+------+------+------+------+\n  a      b      c      d      e      f      g      h");
-        }
-        static void GenerateChessBoard(ChessPieces[,] chessBoard)
-        {
-            for (int j = 0; j < 8; j++)
+            Console.Clear();
+            Console.WriteLine("All moves:");
+            foreach (var item in chess.moves)
             {
-                chessBoard[j, 1] = ChessPieces.Pawn;
-            }
-            chessBoard[0, 0] = ChessPieces.Rook;
-            chessBoard[1, 0] = ChessPieces.Knig;
-            chessBoard[2, 0] = ChessPieces.Bish;
-            chessBoard[3, 0] = ChessPieces.Quen;
-            chessBoard[4, 0] = ChessPieces.King;
-            chessBoard[5, 0] = ChessPieces.Bish;
-            chessBoard[6, 0] = ChessPieces.Knig;
-            chessBoard[7, 0] = ChessPieces.Rook;
-            for (int i = 0; i < 8; i++)
-            {
-                for (int j = 2; j < 6; j++)
-                {
-                    chessBoard[i, j] = ChessPieces.None;
-                }
-            }
-            chessBoard[0, 7] = ChessPieces.Rook;
-            chessBoard[1, 7] = ChessPieces.Knig;
-            chessBoard[2, 7] = ChessPieces.Bish;
-            chessBoard[3, 7] = ChessPieces.Quen;
-            chessBoard[4, 7] = ChessPieces.King;
-            chessBoard[5, 7] = ChessPieces.Bish;
-            chessBoard[6, 7] = ChessPieces.Knig;
-            chessBoard[7, 7] = ChessPieces.Rook;
-            for (int j = 0; j < 8; j++)
-            {
-                chessBoard[j, 6] = ChessPieces.Pawn;
-            }
-
-        }
-        static void MovePiece(ChessPieces[,] chessBoard, ChessPieces piece, int pieceNextPosX, int pieceNextPosY)
-        {
-            if (IsLegalMove(chessBoard, piece, pieceNextPosX, pieceNextPosY))
-            {
-                chessBoard[pieceNextPosX, pieceNextPosY] = piece;
+                Console.Write(item);
             }
         }
-        static ChessPieces FindPiece(ChessPieces[,] chessBoard, int X, int Y)
-        {
-            return chessBoard[X, Y];
-        }
-        static bool IsLegalMove(ChessPieces[,] chessBoard, ChessPieces piece, int nextX, int nextY)
-        {
-            switch (piece)
-            {
-                case ChessPieces.None:
-                    return false;
-                case ChessPieces.Rook: // only in the same row OR the same column as the current position
-                    break;
-                case ChessPieces.Knig: // L position moves - hardest prolly
-                    break;
-                case ChessPieces.Bish: // diagonals
-                    break;
-                case ChessPieces.Quen: // diags + rows/columns
-                    break;
-                case ChessPieces.King: // 1 position in each dirrection
-                    break;
-                case ChessPieces.Pawn: // 1 postion on column, 2 is possible if at start
-                    break;
-                default:
-                    return false;           }
-            return false;
-        }
+        //int pieceCurrPosX = char.Parse(pieceCurrPos.Substring(0, 1)) - 97;
+        //int pieceCurrPosY = int.Parse(pieceCurrPos.Substring(1, 1)) - 1;
     }
 }
 
